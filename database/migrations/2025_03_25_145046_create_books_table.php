3aa1359b571244a0a41e87de1b0a55c4201f9136
Aya-1212 <ayar4407@gmail.com>
2025-03-25 17:38:42 +0200

    Initial commit

---
+++ database/migrations/2025_03_25_145046_create_books_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('image');
+            $table->mediumText('description');
+            $table->string('author');
+            $table->decimial('price',8,2);
+            $table->string('discount');
+            $table->decimal('price_after_discount',8,2);
+            $table->integer('stock_quantity');
+            $table->foreignId('category_id')->constrained('categories')->onDelete('restricted');
+            $table->foreignId('publisher_id')->constrained('publishers')->onDelete('restricted');
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('books');
+    }
+};
---
+++ database/migrations/2025_03_25_150825_create_orders_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->decimal('total_amount',12,2);
+            $table->enum('status',['pending','processed','shipped','completed'])->default('pending');
+            $table->enum('payment_method',['COD'])->default('COD');
+            $table->dateTime('created_at');
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('review_id')->constrained('reviews')->onDelete('cascade');
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
